8c16780c8d6650f7636535fdb396c33c1696e099
AKASH VERMA <aakashverma4102@gmail.com>
2023-01-01 16:57:31 +0530

    finish project

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,18 @@
+$("#thankyou").hide();
 $("button").click(function(event){
     $('#rating').hide();
     $('#thankyou').show();
+    for(var i=1;i<=5;i++){
+        if($("#r"+i).css("background-color")== "rgb(252, 118, 20)"){
+            console.log("working");
+            console.log(i);
+            $(".result>span").text(i);
+            break;
+        }
+    }
+    
+});
+$(".rate").click(function(event){
+    $(".rate").css("background-color","hsl(213, 24%, 24%)");
+    $("#"+event.target.id).css("background-color","#FC7614");
 });
